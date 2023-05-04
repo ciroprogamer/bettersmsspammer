150332a145d766e13de04f775d846c26b22bc7a8
ciroprogamer86 <132161765+ciroprogamer@users.noreply.github.com>
2023-05-04 19:07:32 +0200

    Add files via upload

---
+++ tools/spam.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+#
+# spam: (26/01/2021)
+#
+# [Open Source] - [Código Abierto]
+#
+# Variables y Colores
+#
+PWD=$(pwd)
+OS=$(uname -o)
+USER=$(id -u)
+verde='\033[32m'
+blanco='\033[37m'
+rojo='\033[31m'
+azul='\033[34m'
+negro='\033[0;30m'
+rosa='\033[38;5;207m'
+amarillo='\033[33m'
+morado='\033[35m'
+cian='\033[1;36m'
+magenta='\033[1;35m'
+#
+# Mensaje de Opción Incorrecta
+#
+function Error {
+echo -e "${rojo}
+┌═════════════════════┐
+█ ${blanco}¡OPCIÓN INCORRECTA! ${rojo}█
+└═════════════════════┘
+"${blanco}
+sleep 0.5
+}
+#
+# Banner SETSMS
+#
+function SETSMS {
+	sleep 0.5
+	clear
+echo -e "${verde}
+███████╗███████╗████████╗███████╗███╗   ███╗███████╗
+██╔════╝██╔════╝╚══██╔══╝██╔════╝████╗ ████║██╔════╝
+███████╗█████╗     ██║   ███████╗██╔████╔██║███████╗
+╚════██║██╔══╝     ██║   ╚════██║██║╚██╔╝██║╚════██║
+███████║███████╗   ██║   ███████║██║ ╚═╝ ██║███████║
+╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚══════╝"${blanco}
+}
+#
+# Declarando Funciones
+#
+clear
+source ${PWD}/tools/list.sh
+PHONE='+34621377659'
+NAME='ciro'
+echo -e "${verde}
+┌══════════┐
+█ ${blanco}OBJETIVO ${verde}█
+└══════════┘
+${blanco}
+${NAME} ${verde}=>${azul} ${PHONE}"
+sleep 1
+source /home/ciroprogamer86/SETSMS/numbers/ciro.sh
